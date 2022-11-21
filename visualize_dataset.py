d9432b230eddf2079e13216addf81ffaf0907f6f
Andres Fandos Villanueva <71872419+FandosA@users.noreply.github.com>
2022-11-21 20:42:35 +0100

    Update visualize_dataset.py

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -11,31 +11,35 @@ bounding boxes around the face in the image.
 import os
 import cv2
 import json
+import random
 import numpy as np
 
 
 if __name__ == "__main__":
     
-    images = os.listdir(os.path.join('data', 'labels'))
+    images = os.listdir(os.path.join('data', 'images_resized'))
+    random.shuffle(images)
+    coords = [0, 0, 0, 0]
     
     for image in images: 
         
-        img = cv2.imread(os.path.join('data', 'images_resized', image.split('.')[0] + '.jpg'))
+        img = cv2.imread(os.path.join('data', 'images_resized', image))
         
-        with open(os.path.join('data', 'labels', image), 'r') as f:
-            label = json.load(f)
-        
-        coords = [0, 0, 0, 0]
-        coords[0] = label['shapes'][0]['points'][0][0]
-        coords[1] = label['shapes'][0]['points'][0][1]
-        coords[2] = label['shapes'][0]['points'][1][0]
-        coords[3] = label['shapes'][0]['points'][1][1]
-        
-        cv2.rectangle(img=img,
-                      pt1=tuple(np.array(coords[:2]).astype(int)),
-                      pt2=tuple(np.array(coords[2:]).astype(int)),
-                      color=(0, 0, 255),
-                      thickness=2)
+        if os.path.exists(os.path.join('data', 'labels', image.split('.')[0] + '.json')):
+            
+            with open(os.path.join('data', 'labels', image.split('.')[0] + '.json'), 'r') as f:
+                label = json.load(f)
+            
+            coords[0] = label['shapes'][0]['points'][0][0]
+            coords[1] = label['shapes'][0]['points'][0][1]
+            coords[2] = label['shapes'][0]['points'][1][0]
+            coords[3] = label['shapes'][0]['points'][1][1]
+            
+            cv2.rectangle(img=img,
+                          pt1=tuple(np.array(coords[:2]).astype(int)),
+                          pt2=tuple(np.array(coords[2:]).astype(int)),
+                          color=(0, 0, 255),
+                          thickness=2)
     
         cv2.imshow('Image', img)
         key = cv2.waitKey(1000)
